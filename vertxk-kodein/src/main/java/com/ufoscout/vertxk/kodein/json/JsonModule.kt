d908df97a6f8acd15081471685e9db46a0210031
ufoscout <ufoscout@gmail.com>
2018-05-29 08:49:14 +0200

    Fix bad Json module initialization

--- vertxk-kodein/src/main/java/com/ufoscout/vertxk/kodein/json/JsonModule.kt
+++ vertxk-kodein/src/main/java/com/ufoscout/vertxk/kodein/json/JsonModule.kt
@@ -1,5 +1,9 @@
 package com.ufoscout.vertxk.kodein.json
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.ufoscout.coreutils.json.JacksonJsonSerializerService
 import com.ufoscout.coreutils.json.kotlin.JsonSerializerService
 import com.ufoscout.vertxk.kodein.VertxKModule
@@ -13,13 +17,20 @@ class JsonModule(): VertxKModule {
 
     override fun module() = Kodein.Module {
             bind<JsonSerializerService>() with singleton {
-                JsonSerializerService(JacksonJsonSerializerService(JacksonMapperFactory.mapper))
+                JsonSerializerService(JacksonJsonSerializerService(Json.mapper))
             }
     }
 
     override suspend fun onInit(vertx: Vertx, kodein: Kodein) {
-        Json.mapper = JacksonMapperFactory.mapper
-        Json.prettyMapper = JacksonMapperFactory.prettyMapper
+        initMapper(Json.mapper)
+        initMapper(Json.prettyMapper)
     }
 
+    private fun initMapper(mapper: ObjectMapper) {
+        mapper.registerModule(KotlinModule())
+        mapper.registerModule(JavaTimeModule())
+        mapper.registerModule(Jdk8Module())
+        // mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        // mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+    }
 }
